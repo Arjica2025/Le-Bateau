0a0a1db8f725f4b4881452a98d21e7b675baac8d
Arjica2025 <arjica2022@hotmail.com>
2025-08-10 23:44:08 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker: Instalado');
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
